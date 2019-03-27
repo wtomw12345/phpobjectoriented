e986d648099dffd90f02cae2b7521eceb1f0d738
wtomw12345 <48982965+wtomw12345@users.noreply.github.com>
2019-03-27 09:48:04 +0000

    Add files via upload

---
+++ shareboard/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/shareboard/");
+define("ROOT_URL", "http://localhost/shareboard/");
